df2c422e77be661b6caa52b57233dffcfbd094b8
akyoshid <akyoshid@student.42tokyo.jp>
2025-02-20 22:47:48 +0000

    [ADD] main.c, philo.h & Makefile

---
+++ philo/inc/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/20 22:30:26 by akyoshid          #+#    #+#             */
+/*   Updated: 2025/02/20 22:40:07 by akyoshid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
