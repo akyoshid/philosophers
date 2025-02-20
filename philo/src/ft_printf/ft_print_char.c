56e04e7ce6769ff72212c6e04246eae565bb3222
akyoshid <akyoshid@student.42tokyo.jp>
2025-02-20 23:53:38 +0000

    [ADD] ft_printf

--- philo/src/ft_printf/ft_print_char.c
+++ philo/src/ft_printf/ft_print_char.c
@@ -1,24 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   ft_print_char.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/20 22:30:26 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/02/20 23:51:28 by akyoshid         ###   ########.fr       */
+/*   Created: 2024/08/29 05:21:51 by akyoshid          #+#    #+#             */
+/*   Updated: 2024/12/23 00:34:40 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
+#include "../../inc/ft_printf.h"
 
-# include <string.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <sys/time.h>
-# include <pthread.h>
-# include "ft_printf.h"
+void	ft_print_char(int fd, char const c, int *const cp)
+{
+	if ((write(fd, &c, 1)) == -1)
+		*cp = -1;
+	else
+		(*cp)++;
+}
 
-#endif
+void	ft_print_str(int fd, char const *str, int *const cp)
+{
+	if (str == NULL)
+	{
+		ft_print_str(fd, "(null)", cp);
+		return ;
+	}
+	while (*str != '\0')
+	{
+		ft_print_char(fd, *str, cp);
+		if (*cp == -1)
+			return ;
+		str++;
+	}
+}
