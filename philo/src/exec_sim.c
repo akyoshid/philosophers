16b76956874aab82332788abb3f4a93962e42898
akyoshid <akyoshid@student.42tokyo.jp>
2025-02-25 16:48:35 +0000

    [FIX] bug in _init_philo_data()

--- philo/src/exec_sim.c
+++ philo/src/exec_sim.c
@@ -6,11 +6,22 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 07:42:42 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/02/24 10:12:12 by akyoshid         ###   ########.fr       */
+/*   Updated: 2025/02/25 16:47:55 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
+
+void	_set_first_think_time(
+	t_philo_data *philo_data, t_sim_data *sim_data, int i)
+{
+	if (sim_data->philo_num % 2 != 0 && i == sim_data->philo_num - 1)
+		philo_data[i].first_think_time = sim_data->eat_time * 2;
+	else if (i % 2 == 0)
+		philo_data[i].first_think_time = 0;
+	else
+		philo_data[i].first_think_time = sim_data->eat_time;
+}
 
 void	_init_philo_data(t_philo_data *philo_data, t_sim_data *sim_data)
 {
@@ -21,19 +32,17 @@ void	_init_philo_data(t_philo_data *philo_data, t_sim_data *sim_data)
 	{
 		philo_data[i].philo_id = i + 1;
 		philo_data[i].sim_data = sim_data;
-		philo_data->first_fork = sim_data->fork + i;
-		philo_data->second_fork = sim_data->fork + i + 1;
 		if (i == sim_data->philo_num - 1)
 		{
-			philo_data->first_fork = sim_data->fork + 0;
-			philo_data->second_fork = sim_data->fork + i;
+			philo_data[i].first_fork = sim_data->fork + 0;
+			philo_data[i].second_fork = sim_data->fork + i;
 		}
-		if (sim_data->philo_num % 2 != 0 && i == sim_data->philo_num - 1)
-			philo_data[i].first_think_time = sim_data->eat_time * 2;
-		else if (i % 2 == 0)
-			philo_data[i].first_think_time = 0;
 		else
-			philo_data[i].first_think_time = sim_data->eat_time;
+		{
+			philo_data[i].first_fork = sim_data->fork + i;
+			philo_data[i].second_fork = sim_data->fork + i + 1;
+		}
+		_set_first_think_time(philo_data, sim_data, i);
 		i++;
 	}
 }
@@ -64,11 +73,6 @@ int	_create_philos(t_sim_data *sim_data, t_philo_data *philo_data)
 	return (0);
 }
 
-void	_start_sim(t_sim_data *sim_data)
-{
-	sim_data->start_time = get_current_time_ms();
-}
-
 int	_wait_philos(t_sim_data *sim_data, t_philo_data *philo_data)
 {
 	int	i;
@@ -89,7 +93,7 @@ int	exec_sim(t_sim_data *sim_data)
 	_init_philo_data(philo_data, sim_data);
 	if (_create_philos(sim_data, philo_data) != 0)
 		return (1);
-	_start_sim(sim_data);
+	sim_data->start_time = get_current_time_ms();
 	if (_wait_philos(sim_data, philo_data) != 0)
 		return (1);
 	return (0);
