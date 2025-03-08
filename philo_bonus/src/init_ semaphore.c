30264aedff69246c9fd64c69a4307cc97f7e42cb
akyoshid <akyoshid@student.42tokyo.jp>
2025-03-08 18:09:33 +0000

    [FIX] sem_unlink() in init_semaphore()

--- philo_bonus/src/init_ semaphore.c
+++ philo_bonus/src/init_ semaphore.c
@@ -6,7 +6,7 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 10:59:50 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/03/08 16:48:52 by akyoshid         ###   ########.fr       */
+/*   Updated: 2025/03/08 17:34:51 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,15 @@ int	init_semaphore(t_sim_data *sim_data)
 		print_error(ERR_SEM_OPEN);
 		return (1);
 	}
+	sem_unlink("super_flag");
 	sim_data->fork.s = sem_open(
 			"fork", O_CREAT | O_EXCL, 0666, sim_data->philo_num);
 	if (sim_data->fork.s == SEM_FAILED)
 	{
-		sem_unlink(sim_data->super_flag.s);
 		sem_close(sim_data->super_flag.s);
 		print_error(ERR_SEM_OPEN);
 		return (1);
 	}
+	sem_unlink("fork");
 	return (0);
 }
