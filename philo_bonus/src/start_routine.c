0f4cd07ce18897ba50bb2e8c95d4fe5022b157e6
akyoshid <akyoshid@student.42tokyo.jp>
2025-03-09 21:21:01 +0000

    [SAVE] start_routine.c w/ incorrect semaphore usage

--- philo_bonus/src/start_routine.c
+++ philo_bonus/src/start_routine.c
@@ -6,7 +6,7 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 07:43:44 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/03/09 10:23:48 by akyoshid         ###   ########.fr       */
+/*   Updated: 2025/03/09 21:19:47 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,61 +17,51 @@ void	_check_start_flag(t_sim_data *sim_data)
 	while (1)
 	{
 		usleep(100);
-		sem_wait(&sim_data->super_flag.s);
+		sem_wait(sim_data->super_flag.s);
 		if (sim_data->super_flag.start_flag == true)
 		{
-			sem_post(&sim_data->super_flag.s);
+			sem_post(sim_data->super_flag.s);
 			break ;
 		}
-		sem_post(&sim_data->super_flag.s);
-	}
-}
-
-void	_philo_routine(t_philo_data *philo_data)
-{
-	while (1)
-	{
-		if (philo_eat(philo_data) == STATUS_STOP)
-			break ;
-		if (philo_sleep(philo_data) == STATUS_STOP)
-			break ;
-		if (philo_think(philo_data) == STATUS_STOP)
-			break ;
+		sem_post(sim_data->super_flag.s);
 	}
 }
 
 void	start_philo_routine(t_sim_data *sim_data, t_philo_data *philo_data)
 {
 	_check_start_flag(sim_data);
-	if (sim_data->philo_num == 1)
-		solo_philo_think_and_die(philo_data);
-	else
-		_philo_routine(philo_data);
+	while (1)
+	{
+		if (philo_think(sim_data, philo_data) == STATUS_STOP)
+			break ;
+		if (philo_eat(sim_data, philo_data) == STATUS_STOP)
+			break ;
+		if (philo_sleep(sim_data, philo_data) == STATUS_STOP)
+			break ;
+	}
 }
 
 void	start_waiter_routine(t_sim_data *sim_data, t_philo_data *philo_data)
 {
 	int		i;
-	int		prev_i;
 
 	_check_start_flag(sim_data);
 	i = 0;
-	prev_i = sim_data->philo_num - 1;
 	while (1)
 	{
 		while (1)
 		{
 			usleep(100);
-			sem_wait(philo_data[prev_i].eat_flag.s);
-			if (philo_data[prev_i].eat_flag.f == false)
+			sem_wait(sim_data->philo_count_eating.s);
+			if (sim_data->philo_count_eating.n < sim_data->philo_num / 2)
 				break ;
-			sem_post(philo_data[prev_i].eat_flag.s);
+			sem_post(sim_data->philo_count_eating.s);
 		}
-		sem_post(philo_data[prev_i].eat_flag.s);
 		sem_wait(philo_data[i].eat_flag.s);
-		philo_data[i].eat_flag.f == true;
+		sim_data->philo_count_eating.n++;
+		philo_data[i].eat_flag.f = true;
 		sem_post(philo_data[i].eat_flag.s);
-		prev_i = i;
+		sem_post(sim_data->philo_count_eating.s);
 		i++;
 		if (i == sim_data->philo_num)
 			i = 0;
